558697005871adba6239d1e17c66964e21012b08
Jussstrollin <rulesofsurvival9018@gmail.com>
2026-04-12 00:05:15 +0800

    making code look better

--- Program.cs
+++ Program.cs
@@ -24,10 +24,12 @@ using System;
 using System.IO;
 using System.Text.Json;
 using Spectre.Console;
+using static Program;
+using static StringsStuff;
 
-class Program
+public static class Program
 {
-        static float Essence = 1.0f;
+        public static float Essence = 1.0f;
 
         static DateTime LastGameTick = DateTime.Now;
         static DateTime LastDisplayTick = DateTime.Now;
@@ -39,7 +41,7 @@ class Program
                 public float Essence;
         }
 
-        struct ResourceBP {
+        public struct ResourceBP {
                 public float AlphaAmount;
                 public float AlphaProdPerTick;
                 public float BetaAmount;
@@ -48,7 +50,7 @@ class Program
                 public float GammaProdPerTick;
         }
 
-        struct UpgradeTrackBP {
+        public struct UpgradeTrackBP {
                 // Factory Cost is by default Essence
 
                 public int AlphaFactory;
@@ -74,7 +76,7 @@ class Program
                 public float FactoryOutputUpgradeCost;
         }
 
-        struct GameStateBP {
+        public struct GameStateBP {
                 public float MenuID;
                 public int Progress;
 
@@ -86,7 +88,7 @@ class Program
 
         static ResourceDelta Pending = new ResourceDelta(); // inits all to Zero
 
-        static ResourceBP ResourceWallet = new ResourceBP {
+        public static ResourceBP ResourceWallet = new ResourceBP {
                 AlphaAmount = 0.0f,
                 AlphaProdPerTick = 0.0f,
 
@@ -97,7 +99,7 @@ class Program
                 GammaProdPerTick = 0.0f
         };
 
-        static UpgradeTrackBP UpgradeTrack = new UpgradeTrackBP { // Handles every Upgrades info, but cuurently does too much, will later detach Unrelated stuff
+        public static UpgradeTrackBP UpgradeTrack = new UpgradeTrackBP { // Handles every Upgrades info, but cuurently does too much, will later detach Unrelated stuff
                 AlphaFactory = 0,
                 BetaFactory = 0,
                 GammaFactory = 0,
@@ -123,7 +125,7 @@ class Program
                 FactoryOutputUpgradeCost = 100 // gamma
         };
 
-        static GameStateBP GameState = new GameStateBP {
+        public static GameStateBP GameState = new GameStateBP {
                 MenuID = 0.0f,
                 Progress = 0,
                 Pause = false,
@@ -369,14 +371,7 @@ class Program
 
                 // ============== Game Panel Stuff ==================== //
 
-                var GameStatPanel = new Panel(
-                        $"[white][/]\n" +
-                        $"[cyan]Essence : {Essence}[/]\n" +
-                        $"[white][/]\n" +
-                        $"[yellow]Alpha : {ResourceWallet.AlphaAmount}[/]\n" +
-                        $"[blue]Beta : {ResourceWallet.BetaAmount}[/]\n" +
-                        $"[green]Gamma : {ResourceWallet.GammaAmount}[/]\n"
-                );
+                var GameStatPanel = new Panel(StringsStuff.GamePanelStats);
                 GameStatPanel.Width = 70;
                 GameStatPanel.Height = 16;
                 GameStatPanel.Header = new PanelHeader(" Game : Stat Menu");
@@ -443,27 +438,7 @@ class Program
                 }
 
                 // ---- The UI ---- //
-                var ShopMenu = new Panel(
-                        $"Here, you can Buy more Factories, Essence Upgrades (and Factory upgrades in the future).\n" +
-                        $"\n" +
-                        $"\n" +
-                        $" > Factories < \n" +
-                        $"  -> [yellow] Alpha Factory [/] Press 1 to see\n" +
-                        $"  -> [blue] Beta Factory [/] Press 2 to see\n" +
-                        $"  -> [green] Gamma Factory [/] Press 3 to see\n" +
-                        $"\n" +
-                        $"\n" +
-                        $" > [blue]Essence[/] Upgrades < \n" +
-                        $"\n" +
-                        $" -> [cyan]Essence Base Production[/] Press 4 to see\n" +
-                        $" -> [cyan]Essence Multiplier[/] Press 5 to see\n" +
-                        $"\n" +
-                        $"\n" +
-                        $" > Factory Upgrade < \n" +
-                        $"\n" +
-                        $" -> Make Factory input system safer Press 6 to see \n" +
-                        $" -> Make Factory Production line Smarter Press 7 to see \n"
-                );
+                var ShopMenu = new Panel(StringsStuff.ShopMainPanel);
 
                 var ShopEntryPanel0 = new Panel(
                         $"No entry Chosen"
@@ -481,103 +456,13 @@ class Program
                         $" Cannot Afford Factory! "
                 );
 
-                var ShopEntryPanel1 = new Panel(
-                        $"[yellow] Alpha Factory [/]\n" +
-                        $"\n" +
-                        $"Description : \n" +
-                        $" - A factory that Consumes [cyan]1 Essence[/] to produce [yellow]1 Alpha[/] Per tick.\n" +
-                        $"\n" +
-                        $"Cost : [cyan]{UpgradeTrack.AlphaFactoryCost}[/]\n" +
-                        $"" +
-                        $"You currently have : [yellow]{UpgradeTrack.AlphaFactory}[/] Factories\n" +
-                        $"\n" +
-                        $"Press ENTER to Purchase\n" +
-                        $"Press B to Go back\n"
-                );
-
-                var ShopEntryPanel2 = new Panel(
-                        $"[blue] Beta Factory [/]\n" +
-                        $"\n" +
-                        $"Description : \n" +
-                        $" - A factory that Consumes [yellow]1 Alpha[/] to produce [blue]1 Beta[/] Per tick.\n" +
-                        $"\n" +
-                        $"Cost : [cyan]{UpgradeTrack.BetaFactoryCost}[/]\n" +
-                        $"" +
-                        $"You currently have : [blue]{UpgradeTrack.BetaFactory}[/] Factories\n" +
-                        $"\n" +
-                        $"Press ENTER to Purchase\n" +
-                        $"Press B to Go back\n"
-                );
-
-                var ShopEntryPanel3 = new Panel(
-                        $"[green] Gamma Factory [/]\n" +
-                        $"\n" +
-                        $"Description : \n" +
-                        $" - A factory that Consumes [yellow]1 Alpha[/] and [blue]1 Beta [/]to produce [green]1 Gamma[/] Per tick.\n" +
-                        $"\n" +
-                        $"Cost : [cyan]{UpgradeTrack.GammaFactoryCost}[/]\n" +
-                        $"" +
-                        $"You currently have : [green]{UpgradeTrack.GammaFactory}[/] Factories\n" +
-                        $"\n" +
-                        $"Press ENTER to Purchase\n" +
-                        $"Press B to Go back\n"
-                );
-
-                var ShopEntryPanel4 = new Panel(
-                        $"[cyan] Essence Base Production [/]\n" +
-                        $"\n" +
-                        $"Description : \n" +
-                        $" - [cyan]Essence[/] Is produced at the rate of Base multiplied by a Multiplier ( E = Base*multiplier ), buying this adds +1 Essence per tick times {UpgradeTrack.EssenceMultiplierBought}\n" +
-                        $"\n" +
-                        $"Cost : [yellow]{UpgradeTrack.EssenceBaseCost} Alpha[/]\n" +
-                        $"" +
-                        $"You currently have : [cyan]{UpgradeTrack.EssenceBaseBought} Base Essence Production[/]\n" +
-                        $"\n" +
-                        $"Press ENTER to Purchase\n" +
-                        $"Press B to Go back\n"
-                );
-
-                var ShopEntryPanel5 = new Panel(
-                        $"[cyan] Essence Multiplier [/]\n" +
-                        $"\n" +
-                        $"Description : \n" +
-                        $" - Adds A Multiplier for [cyan]Essence[/] Production\n" +
-                        $"\n" +
-                        $"Cost : [blue]{UpgradeTrack.EssenceMultiplierCost} Beta[/]\n" +
-                        $"" +
-                        $"You currently have : [cyan]{UpgradeTrack.EssenceMultiplierBought} Essence Multiplier[/]\n" +
-                        $"\n" +
-                        $"Press ENTER to Purchase\n" +
-                        $"Press B to Go back\n"
-                );
-
-                var ShopEntryPanel6 = new Panel(
-                        $"[cyan] Factory Input mechanism [/]\n" +
-                        $"\n" +
-                        $"Description : \n" +
-                        $" - Improving the Input Mechanism of all Factory, improving and reducing needed Resource input by 5%\n" +
-                        $"\n" +
-                        $"Cost : [green]{UpgradeTrack.FactoryInputUpgradeCost} Gamma[/]\n" +
-                        $"" +
-                        $"You currently have : [white]{UpgradeTrack.FactoryInputUpgradeBought} Upgrades Bought[/]\n" +
-                        $"\n" +
-                        $"Press ENTER to Purchase\n" +
-                        $"Press B to Go back\n"
-                );
-
-                var ShopEntryPanel7 = new Panel(
-                        $"[cyan] Factory Line Performance Optimisation [/]\n" +
-                        $"\n" +
-                        $"Description : \n" +
-                        $" - Improving the Factory Line to gain ~10% Output for the same Input some said 'why are we using an inefficient one in the first place?' \n" +
-                        $"\n" +
-                        $"Cost : [green]{UpgradeTrack.FactoryOutputUpgradeCost} Gamma[/]\n" +
-                        $"" +
-                        $"You currently have : [white]{UpgradeTrack.FactoryOutputUpgradeBought} Upgrades Bought[/]\n" +
-                        $"\n" +
-                        $"Press ENTER to Purchase\n" +
-                        $"Press B to Go back\n"
-                );
+                var ShopEntryPanel1 = new Panel(StringsStuff.ShopEntryPanel1);
+                var ShopEntryPanel2 = new Panel(StringsStuff.ShopEntryPanel2);
+                var ShopEntryPanel3 = new Panel(StringsStuff.ShopEntryPanel3);
+                var ShopEntryPanel4 = new Panel(StringsStuff.ShopEntryPanel4);
+                var ShopEntryPanel5 = new Panel(StringsStuff.ShopEntryPanel5);
+                var ShopEntryPanel6 = new Panel(StringsStuff.ShopEntryPanel6);
+                var ShopEntryPanel7 = new Panel(StringsStuff.ShopEntryPanel7);
 
                 ShopMenu.Header = new PanelHeader(" Shop Menu ");
                 ShopMenu.Width = 67;
@@ -905,4 +790,140 @@ class Program
                         GameState.MenuID = 0.0f;
                 }
         }
+}
+
+public static class StringsStuff
+{
+        public static string GamePanelStats =>
+                $"[white][/]\n" +
+                $"[cyan]Essence : {Essence}[/]\n" +
+                $"[white][/]\n" +
+                $"[yellow]Alpha : {ResourceWallet.AlphaAmount}[/]\n" +
+                $"[blue]Beta : {ResourceWallet.BetaAmount}[/]\n" +
+                $"[green]Gamma : {ResourceWallet.GammaAmount}[/]\n"
+        ;
+
+        public static string ShopMainPanel =>
+                $"Here, you can Buy more Factories, Essence Upgrades (and Factory upgrades in the future).\n" +
+                $"\n" +
+                $"\n" +
+                $" > Factories < \n" +
+                $"  -> [yellow] Alpha Factory [/] Press 1 to see\n" +
+                $"  -> [blue] Beta Factory [/] Press 2 to see\n" +
+                $"  -> [green] Gamma Factory [/] Press 3 to see\n" +
+                $"\n" +
+                $"\n" +
+                $" > [blue]Essence[/] Upgrades < \n" +
+                $"\n" +
+                $" -> [cyan]Essence Base Production[/] Press 4 to see\n" +
+                $" -> [cyan]Essence Multiplier[/] Press 5 to see\n" +
+                $"\n" +
+                $"\n" +
+                $" > Factory Upgrade < \n" +
+                $"\n" +
+                $" -> Make Factory input system safer Press 6 to see \n" +
+                $" -> Make Factory Production line Smarter Press 7 to see \n"
+        ;
+
+        public static string ShopEntryPanel0 =>
+
+        ;
+
+        public static string ShopEntryPanel1 =>
+                $"[yellow] Alpha Factory [/]\n" +
+                $"\n" +
+                $"Description : \n" +
+                $" - A factory that Consumes [cyan]1 Essence[/] to produce [yellow]1 Alpha[/] Per tick.\n" +
+                $"\n" +
+                $"Cost : [cyan]{UpgradeTrack.AlphaFactoryCost}[/]\n" +
+                $"" +
+                $"You currently have : [yellow]{UpgradeTrack.AlphaFactory}[/] Factories\n" +
+                $"\n" +
+                $"Press ENTER to Purchase\n" +
+                $"Press B to Go back\n"
+        ;
+
+        public static string ShopEntryPanel2 =>
+                $"[blue] Beta Factory [/]\n" +
+                $"\n" +
+                $"Description : \n" +
+                $" - A factory that Consumes [yellow]1 Alpha[/] to produce [blue]1 Beta[/] Per tick.\n" +
+                $"\n" +
+                $"Cost : [cyan]{UpgradeTrack.BetaFactoryCost}[/]\n" +
+                $"" +
+                $"You currently have : [blue]{UpgradeTrack.BetaFactory}[/] Factories\n" +
+                $"\n" +
+                $"Press ENTER to Purchase\n" +
+                $"Press B to Go back\n"
+        ;
+
+        public static string ShopEntryPanel3 =>
+                $"[green] Gamma Factory [/]\n" +
+                $"\n" +
+                $"Description : \n" +
+                $" - A factory that Consumes [yellow]1 Alpha[/] and [blue]1 Beta [/]to produce [green]1 Gamma[/] Per tick.\n" +
+                $"\n" +
+                $"Cost : [cyan]{UpgradeTrack.GammaFactoryCost}[/]\n" +
+                $"" +
+                $"You currently have : [green]{UpgradeTrack.GammaFactory}[/] Factories\n" +
+                $"\n" +
+                $"Press ENTER to Purchase\n" +
+                $"Press B to Go back\n"
+        ;
+
+        public static string ShopEntryPanel4 =>
+                $"[cyan] Essence Base Production [/]\n" +
+                $"\n" +
+                $"Description : \n" +
+                $" - [cyan]Essence[/] Is produced at the rate of Base multiplied by a Multiplier ( E = Base*multiplier ), buying this adds +1 Essence per tick times {UpgradeTrack.EssenceMultiplierBought}\n" +
+                $"\n" +
+                $"Cost : [yellow]{UpgradeTrack.EssenceBaseCost} Alpha[/]\n" +
+                $"" +
+                $"You currently have : [cyan]{UpgradeTrack.EssenceBaseBought} Base Essence Production[/]\n" +
+                $"\n" +
+                $"Press ENTER to Purchase\n" +
+                $"Press B to Go back\n"
+        ;
+
+        public static string ShopEntryPanel5 =>
+                $"[cyan] Essence Multiplier [/]\n" +
+                $"\n" +
+                $"Description : \n" +
+                $" - Adds A Multiplier for [cyan]Essence[/] Production\n" +
+                $"\n" +
+                $"Cost : [blue]{UpgradeTrack.EssenceMultiplierCost} Beta[/]\n" +
+                $"" +
+                $"You currently have : [cyan]{UpgradeTrack.EssenceMultiplierBought} Essence Multiplier[/]\n" +
+                $"\n" +
+                $"Press ENTER to Purchase\n" +
+                $"Press B to Go back\n"
+        ;
+
+        public static string ShopEntryPanel6 =>
+                $"[cyan] Factory Input mechanism [/]\n" +
+                $"\n" +
+                $"Description : \n" +
+                $" - Improving the Input Mechanism of all Factory, improving and reducing needed Resource input by 5%\n" +
+                $"\n" +
+                $"Cost : [green]{UpgradeTrack.FactoryInputUpgradeCost} Gamma[/]\n" +
+                $"" +
+                $"You currently have : [white]{UpgradeTrack.FactoryInputUpgradeBought} Upgrades Bought[/]\n" +
+                $"\n" +
+                $"Press ENTER to Purchase\n" +
+                $"Press B to Go back\n"
+        ;
+
+        public static string ShopEntryPanel7 =>
+                $"[cyan] Factory Line Performance Optimisation [/]\n" +
+                $"\n" +
+                $"Description : \n" +
+                $" - Improving the Factory Line to gain ~10% Output for the same Input some said 'why are we using an inefficient one in the first place?' \n" +
+                $"\n" +
+                $"Cost : [green]{UpgradeTrack.FactoryOutputUpgradeCost} Gamma[/]\n" +
+                $"" +
+                $"You currently have : [white]{UpgradeTrack.FactoryOutputUpgradeBought} Upgrades Bought[/]\n" +
+                $"\n" +
+                $"Press ENTER to Purchase\n" +
+                $"Press B to Go back\n"
+        ;
 }
